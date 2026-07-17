8536626745e04e1d87875adccc82941e63bed550
masomeh <ghasembigloo5@gmail.com>
2026-07-17 19:23:52 +0330

    connect urls to page
    I use dynamic url to connected to page

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from website.views import home
+from website.views import contact,index, about
 
 urlpatterns = [
-    path('', home )
+    path('', index,name= 'index' ),
+    path('contact', contact, name='contact'),
+    path('about', about, name='about')
 ]
--- website/views.py
+++ website/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def home(requstes):
+def index(requstes):
     return render(requstes,'website/index.html')
+
+def about(requstes):
+    return render(requstes,'website/about.html')
+
+def contact(requstes):
+    return render(requstes,'website/contact.html')
